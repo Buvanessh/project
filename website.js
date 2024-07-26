da0ab3a11e73f3e27093080d4bf03cabd3feeffd
Buvanessh <160506867+Buvanessh@users.noreply.github.com>
2024-07-26 20:22:34 +0530

    Add files via upload

---
+++ website.js
@@ -0,0 +1,12 @@
+let body =document.getElementById("menu")
+
+var icon=document.getElementById("icon")
+icon.addEventListener("click",()=>{
+    
+    second.style.display="block"
+})
+ var click=document.getElementById("placeorder")
+ click.addEventListener("click",()=>{
+    alert("Your order placed successfully")
+ })
+
